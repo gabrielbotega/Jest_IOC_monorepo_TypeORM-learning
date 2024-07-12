0bd8f083fff1e58c0f806d8522347c6340b51b4b
Gabriel Botega <gabriel.botega@engenharia.ufjf.br>
2024-07-11 21:57:32 -0300

    Test Dependency Injection

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,20 @@
 export class Person {
+  fruit?: IFruit;
+  constructor(fruit?: IFruit) {
+    this.fruit = fruit;
+  }
+
   public myName(): string {
     return "Felipe";
   }
 }
+
+interface IFruit {
+  fruitName: string;
+}
+
+export class Goiaba implements IFruit {
+  fruitName: string = "Goiaba";
+}
+
+console.log(new Person({ fruitName: "Maça" }).fruit?.fruitName);
