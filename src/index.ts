b060acc712abdba4609e7d474bb3e2f518d26c6a
Gabriel Botega <gabriel.botega@engenharia.ufjf.br>
2024-07-13 18:29:12 -0300

    Setup typeORM

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,20 @@
-export class Person {
-  fruit?: IFruit;
-  constructor(fruit?: IFruit) {
-    this.fruit = fruit;
-  }
+import { AppDataSource } from "./data-source"
+import { User } from "./entity/User"
 
-  public myName(): string {
-    return "Felipe";
-  }
-}
+AppDataSource.initialize().then(async () => {
 
-interface IFruit {
-  fruitName: string;
-}
+    console.log("Inserting a new user into the database...")
+    const user = new User()
+    user.firstName = "Timber"
+    user.lastName = "Saw"
+    user.age = 25
+    await AppDataSource.manager.save(user)
+    console.log("Saved a new user with id: " + user.id)
 
-export class Goiaba implements IFruit {
-  fruitName: string = "Goiaba";
-}
+    console.log("Loading users from the database...")
+    const users = await AppDataSource.manager.find(User)
+    console.log("Loaded users: ", users)
 
-console.log(new Person({ fruitName: "Maça" }).fruit?.fruitName);
+    console.log("Here you can setup and run express / fastify / any other framework.")
+
+}).catch(error => console.log(error))
