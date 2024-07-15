77bfe9c0fbb952bd26aaa52e80f69faaded4c553
Gabriel Botega <gabriel.botega@engenharia.ufjf.br>
2024-07-15 20:40:32 -0300

    Update: updating inversify TYPES

---
+++ src/constants/symbols.constants.ts
@@ -0,0 +1,19 @@
+export default {
+  v1: {
+    Interfaces: {
+      IUser: Symbol("IUser"),
+    },
+    Services: {
+      userService: Symbol("userService"),
+    },
+    Controllers: {
+      userController: Symbol("userController"),
+    },
+    Dao: {
+      userDao: Symbol("userDao"),
+    },
+    Dto: {
+      userDto: Symbol("userDto"),
+    },
+  },
+};
