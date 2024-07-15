14d0d4e9ee2abefe8596d59735824d2ddd881fac
Gabriel Botega <gabriel.botega@engenharia.ufjf.br>
2024-07-15 20:39:22 -0300

    Fix: compatibility between IUser and User model entity

--- src/models/dto/user.dto.ts
+++ src/models/dto/user.dto.ts
@@ -1,7 +1,16 @@
-export class userDto {
+import { injectable } from "inversify";
+
+@injectable()
+export class UserDto {
   public id?: string;
 
   public firstName: string;
 
   public lastName: string;
+
+  public age: number;
+
+  public createdAt?: Date;
+
+  public updatedAt?: Date;
 }
