ae2d6efdd696ab40d7eadb4f9457e950057296c5
Gabriel Botega <gabriel.botega@engenharia.ufjf.br>
2024-07-15 20:41:30 -0300

    Fix: Fix inversify.config DTO binding

--- src/inversify.config.ts
+++ src/inversify.config.ts
@@ -1,16 +1,16 @@
 import { Container } from "inversify";
-import injectionTokensConstants from "./constants/injection.tokens.constants";
+import TYPES from "./constants/symbols.constants";
 import { UserService } from "./service/v1/users.service";
 import { UserDao } from "./service/dao/users.dao";
 import UserController from "./controller/users.controller";
+import { UserDto } from "./models/dto/user.dto";
 
 const myContainer = new Container();
+myContainer.bind<UserService>(TYPES.v1.Services.userService).to(UserService);
+myContainer.bind<UserDao>(TYPES.v1.Dao.userDao).to(UserDao);
 myContainer
-  .bind<UserService>(injectionTokensConstants.v1.Services.userService)
-  .to(UserService);
-myContainer.bind<UserDao>(injectionTokensConstants.v1.Dao.userDao).to(UserDao);
-myContainer
-  .bind<UserController>(injectionTokensConstants.v1.Controllers.userController)
+  .bind<UserController>(TYPES.v1.Controllers.userController)
   .to(UserController);
+myContainer.bind<UserDto>(TYPES.v1.Dto.userDto).to(UserDto);
 
 export { myContainer };
